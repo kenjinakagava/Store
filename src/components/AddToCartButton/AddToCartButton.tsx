afc275dd24377c7c9b683be58fd73e79e0541189
Kenji.N <1314webdev@gmail.com>
2023-05-01 10:24:37 -0300

    Fix cart product counter

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -11,7 +11,6 @@ interface AddToCartButtonProps extends CartItem {
 
 const AddToCartButton = (props: AddToCartButtonProps) => {
   const [showToast, setShowToast] = useState(false);
-  console.log(showToast);
   const handleToast = () => {
     setShowToast(true);
     setTimeout(() => setShowToast(false), 3000);
@@ -22,6 +21,7 @@ const AddToCartButton = (props: AddToCartButtonProps) => {
   const { t } = useTranslation();
 
   const addToCart = (item: CartItem) => {
+    // create newItem object using the props passed to this (addToCartButton) component
     const newItem: CartItem = {
       id: item.id,
       title: item.title,
@@ -30,19 +30,27 @@ const AddToCartButton = (props: AddToCartButtonProps) => {
       // if not defined (like in the homescreen) add one unit of the product
       quantity: props.quantity ? props.quantity : 1,
     };
+    // find whether the item our "add to cart button" is referring to is already on the cart
+    // if so get it's id on the cartItems array
     const existingItemIndex = cartItems.findIndex(
       (cartItem) => cartItem.id === newItem.id
     );
+    // if the product already exists on the cart
+    // we make this distinction so we don't overwrite the product with a copy of itself, this way we can keep track of the quantity
     if (existingItemIndex !== -1) {
+      // create a new array with the current cartItems
       const updatedCartItems = [...cartItems];
-      // if not defined (like in the homescreen) add one unit of the product
+      // change the quantity of the cartItem this button reefers
       updatedCartItems[existingItemIndex].quantity += props.quantity
         ? props.quantity
-        : 1;
+        : // in the homepage we don't show the quantity selector, so we add one unit every time
+          1;
       setCartItems(updatedCartItems);
     } else {
+      // if it's a new product add the new product to the cart
       setCartItems((previousCartItems) => [...previousCartItems, newItem]);
     }
+    // opens the toast after clicking the add to cart button
     handleToast();
   };
   return (
